081b15b0e7e36aea8e25b1ce717b2d38fe8cdb43
adamnemecek <adamnemecek@gmail.com>
2022-03-05 20:49:52 -0800

    stuff

--- Sources/BitTools/GPUBitArray.swift
+++ Sources/BitTools/GPUBitArray.swift
@@ -15,52 +15,6 @@ extension GPUBitArray: Sequence {
     }
 }
 
-extension MutableCollection where Element: FixedWidthInteger {
-    mutating func formBitOp<C>(
-        _ other: C,
-        count: Int,
-        op: (Element, Element) -> Element
-    ) -> Int where C: Collection, C.Element == Element {
-        var bitCount = 0
-
-        var i = self.startIndex
-        var j = other.startIndex
-
-        for _ in 0..<count {
-            let new = op(self[i], other[j])
-            bitCount += new.nonzeroBitCount
-            self[i] = new
-
-            i = self.index(after: i)
-            j = other.index(after: j)
-        }
-        return bitCount
-    }
-
-    mutating func bitOp<A, B>(
-        _ a: A,
-        _ b: B,
-        count: Int,
-        op: (Element, Element) -> Element
-    ) -> Int where A: Collection, A.Element == Element, B: Collection, B.Element == Element {
-        var bitCount = 0
-
-        var i = self.startIndex
-        var j = a.startIndex
-        var k = b.startIndex
-
-        for _ in 0..<count {
-            let new = op(a[j], b[k])
-            bitCount += new.nonzeroBitCount
-            self[i] = new
-
-            i = self.index(after: i)
-            j = a.index(after: j)
-            k = b.index(after: k)
-        }
-        return bitCount
-    }
-}
 
 extension GPUBitArray : SetAlgebra {
     public typealias Element = Int
--- Sources/BitTools/UnsafePtrExt.swift
+++ Sources/BitTools/UnsafePtrExt.swift
@@ -1,4 +1,51 @@
 
+extension MutableCollection where Element: FixedWidthInteger {
+    mutating func formBitOp<C>(
+        _ other: C,
+        count: Int,
+        op: (Element, Element) -> Element
+    ) -> Int where C: Collection, C.Element == Element {
+        var bitCount = 0
+
+        var i = self.startIndex
+        var j = other.startIndex
+
+        for _ in 0..<count {
+            let new = op(self[i], other[j])
+            bitCount += new.nonzeroBitCount
+            self[i] = new
+
+            i = self.index(after: i)
+            j = other.index(after: j)
+        }
+        return bitCount
+    }
+
+    mutating func bitOp<A, B>(
+        _ a: A,
+        _ b: B,
+        count: Int,
+        op: (Element, Element) -> Element
+    ) -> Int where A: Collection, A.Element == Element, B: Collection, B.Element == Element {
+        var bitCount = 0
+
+        var i = self.startIndex
+        var j = a.startIndex
+        var k = b.startIndex
+
+        for _ in 0..<count {
+            let new = op(a[j], b[k])
+            bitCount += new.nonzeroBitCount
+            self[i] = new
+
+            i = self.index(after: i)
+            j = a.index(after: j)
+            k = b.index(after: k)
+        }
+        return bitCount
+    }
+}
+
 
 extension UnsafeMutableBufferPointer where Element == UInt64 {
     public func formUnion(_ other: UnsafeBufferPointer<Element>, count: Int) -> Int {
